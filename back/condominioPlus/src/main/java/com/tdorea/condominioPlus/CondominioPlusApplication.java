eaa29f6f4647021b4c969981812271f2d7114328
thiagodorea <thiago.dorea@hotmail.com>
2022-03-21 21:54:53 -0300

    seting application properties

---
+++ back/condominioPlus/src/main/java/com/tdorea/condominioPlus/CondominioPlusApplication.java
@@ -0,0 +1,13 @@
+package com.tdorea.condominioPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominioPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominioPlusApplication.class, args);
+	}
+
+}
---
+++ back/condominioPlus/src/test/java/com/tdorea/condominioPlus/CondominioPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdorea.condominioPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominioPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
